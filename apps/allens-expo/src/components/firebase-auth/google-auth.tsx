e436a8bdb265cb900ee422b897de1e6e006dbbab
HarriMac <wootak95@gmail.com>
2025-12-16 20:37:08 +0900

    update google auth file

--- apps/allens-expo/src/components/firebase-auth/google-auth.tsx
+++ apps/allens-expo/src/components/firebase-auth/google-auth.tsx
@@ -1,9 +1,44 @@
 import {Dimensions, StyleSheet, View} from 'react-native';
 import React from 'react';
 
-import {GoogleSigninButton} from '@react-native-google-signin/google-signin';
+import {
+    GoogleSignin,
+    GoogleSigninButton,
+    isErrorWithCode,
+    isSuccessResponse,
+    statusCodes,    
+} from '@react-native-google-signin/google-signin';
+
+GoogleSignin.configure();
 
 const GoogleAuth = () => {
+    const signIn = async () => {
+        try {
+            await GoogleSignin.hasPlayServices();
+            const response = await GoogleSignin.signIn();
+            if (isSuccessResponse(response)) {
+                setState({userInfo: response.data});
+            } else {
+                //sing-in was cancelled
+            }
+        } catch (error) {
+            if (isErrorWithCode(error)) {
+                switch (error.code) {
+                    case statusCodes.IN_PROGRESS:
+                        // operation (e.g. sign in) is in progress already
+                        break;
+                    case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+                        // Android only, play services not available or outdated
+                        break;
+                    default:
+                        // some other error happened
+                }
+            } else {
+                // error not related to Google Signin
+            }  
+        }
+    };
+
     return(
         <View 
             style={{
@@ -17,7 +52,7 @@ const GoogleAuth = () => {
                 size={GoogleSigninButton.Size.Wide}
                 color={GoogleSigninButton.Color.Dark}
                 onPress={() => {
-                    alert(1);
+                    signIn();
                     //initiate Google Sign-In process
                 }}
                 //disabled={isInProgress}
